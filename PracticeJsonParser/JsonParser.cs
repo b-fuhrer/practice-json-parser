555fd6e90690bbf2c0cf40ac312c2ab274881728
b-fuhrer <b-fuhrer@users.noreply.github.com>
2025-11-24 21:13:47 +0100

    refactor: migrate parser logic to UTF-8 bytes and fix index passing to sub-parsers

--- PracticeJsonParser/JsonParser.cs
+++ PracticeJsonParser/JsonParser.cs
@@ -1,40 +1,41 @@
 ﻿namespace PracticeJsonParser;
+using System.Text;
 
 public static class JsonParser
 {
     // not implemented yet
-    public static JsonValue Parse(ReadOnlySpan<char> jsonText)
+    public static JsonValue Parse(ReadOnlySpan<byte> jsonText)
     {
         var currentIndex = 0;
         
         return JsonNull.Instance;
     }
 
-    private static JsonResult<JsonValue> ParseIntoValue(ReadOnlySpan<char> jsonText, int currentIndex)
+    private static JsonResult<JsonValue> ParseIntoValue(ReadOnlySpan<byte> jsonText, int currentIndex)
     {
         var newIndex = SkipWhitespace(jsonText, currentIndex);
         
         if (newIndex == jsonText.Length) return JsonResult<JsonValue>.Err(JsonErrorType.EndOfFile, newIndex);
         
-        var currentCharacter = jsonText[currentIndex];
+        var nextCharacter = jsonText[newIndex];
 
-        var parsedValue = currentCharacter switch
+        var parsedValue = nextCharacter switch
         {
-            'n' => ParseNull(jsonText, currentIndex),
-            't' or 'f' => ParseBool(jsonText, currentIndex),
-            '-' or >= '0' and <= '9' => ParseNumber(jsonText, currentIndex),
-            '"' => ParseString(jsonText, currentIndex),
-            '[' => ParseArray(jsonText, currentIndex),
-            '{' => ParseObject(jsonText, currentIndex),
+            (byte)'n' => ParseNull(jsonText, newIndex),
+            (byte)'t' or (byte)'f' => ParseBool(jsonText, newIndex),
+            (byte)'-' or >= (byte)'0' and <= (byte)'9' => ParseNumber(jsonText, newIndex),
+            (byte)'"' => ParseString(jsonText, newIndex),
+            (byte)'[' => ParseArray(jsonText, newIndex),
+            (byte)'{' => ParseObject(jsonText, newIndex),
             _ => JsonResult<JsonValue>.Err(
-                JsonErrorType.InvalidCharacter, $"Invalid character: {currentCharacter}", newIndex
+                JsonErrorType.InvalidCharacter, $"Invalid character: {nextCharacter}", newIndex
             )
         };
 
         return parsedValue;
     }
     
-    private static int SkipWhitespace(ReadOnlySpan<char> jsonText, int currentIndex)
+    private static int SkipWhitespace(ReadOnlySpan<byte> jsonText, int currentIndex)
     {
         while (currentIndex < jsonText.Length)
         {
@@ -42,7 +43,7 @@ public static class JsonParser
             
             // treats all control characters (ASCII < 32) as whitespace
             // includes JSON whitespace characters: '\t', '\n', '\r'
-            if (currentCharacter > ' ') return currentIndex;
+            if (currentCharacter > (byte)' ') return currentIndex;
             
             currentIndex++;
         }
@@ -50,98 +51,63 @@ public static class JsonParser
         return currentIndex;
     }
 
-    private static JsonResult<JsonNull> ParseNull(ReadOnlySpan<char> jsonText, int currentIndex)
+    private static JsonResult<JsonNull> ParseNull(ReadOnlySpan<byte> jsonText, int currentIndex)
     {
         const int nullLength = 4;
 
         if (currentIndex + nullLength > jsonText.Length)
-        {
             return JsonResult<JsonNull>.Err(JsonErrorType.EndOfFile, currentIndex);
-        }
 
-        var possibleNullSlice = jsonText.Slice(currentIndex, nullLength);
+        var slice = jsonText.Slice(currentIndex, nullLength);
 
-        return possibleNullSlice switch
-        {
-            "null" => JsonResult<JsonNull>.Ok(JsonNull.Instance, currentIndex + nullLength),
-            _ => JsonResult<JsonNull>.Err(
-                JsonErrorType.InvalidSyntax, $"Expected 'null', received '{possibleNullSlice}'", currentIndex
-            )
-        };
+        return slice.SequenceEqual("null"u8)
+            ? JsonResult<JsonNull>.Ok(JsonNull.Instance, currentIndex + nullLength)
+            : JsonResult<JsonNull>.Err(
+                JsonErrorType.InvalidSyntax, $"Expected 'null', received '{Encoding.UTF8.GetString(slice)}'", currentIndex
+            );
     }
 
-    private static JsonResult<JsonBool> ParseTrue(ReadOnlySpan<char> jsonText, int currentIndex)
+    private static JsonResult<JsonBool> ParseTrue(ReadOnlySpan<byte> jsonText, int currentIndex)
     {
         const int trueLength = 4;
 
         if (currentIndex + trueLength > jsonText.Length)
-        {
             return JsonResult<JsonBool>.Err(JsonErrorType.EndOfFile, currentIndex);
-        }
 
-        var possibleTrueSlice = jsonText.Slice(currentIndex, trueLength);
+        var slice = jsonText.Slice(currentIndex, trueLength);
 
-        return possibleTrueSlice switch
-        {
-            "true" => JsonResult<JsonBool>.Ok(new JsonBool(true), currentIndex + trueLength),
-            _ => JsonResult<JsonBool>.Err(
-                JsonErrorType.InvalidSyntax, $"Expected 'true', received '{possibleTrueSlice}'", currentIndex
-            )
-        };
+        return slice.SequenceEqual("true"u8)
+            ? JsonResult<JsonBool>.Ok(new JsonBool(true), currentIndex + trueLength)
+            : JsonResult<JsonBool>.Err(
+                JsonErrorType.InvalidSyntax, $"Expected 'true', received '{Encoding.UTF8.GetString(slice)}'", currentIndex
+            );
     }
-    
-    private static JsonResult<JsonBool> ParseFalse(ReadOnlySpan<char> jsonText, int currentIndex)
+
+    private static JsonResult<JsonBool> ParseFalse(ReadOnlySpan<byte> jsonText, int currentIndex)
     {
         const int falseLength = 5;
 
         if (currentIndex + falseLength > jsonText.Length)
-        {
             return JsonResult<JsonBool>.Err(JsonErrorType.EndOfFile, currentIndex);
-        }
 
-        var possibleFalseSlice = jsonText.Slice(currentIndex, falseLength);
+        var slice = jsonText.Slice(currentIndex, falseLength);
 
-        return possibleFalseSlice switch
-        {
-            "false" => JsonResult<JsonBool>.Ok(new JsonBool(false), currentIndex + falseLength),
-            _ => JsonResult<JsonBool>.Err(
-                JsonErrorType.InvalidSyntax, $"Expected 'false', received '{possibleFalseSlice}'", currentIndex
-            )
-        };
+        return slice.SequenceEqual("false"u8)
+            ? JsonResult<JsonBool>.Ok(new JsonBool(false), currentIndex + falseLength)
+            : JsonResult<JsonBool>.Err(
+                JsonErrorType.InvalidSyntax, $"Expected 'false', received '{Encoding.UTF8.GetString(slice)}'",
+                currentIndex
+            );
     }
 
-    /*
-    // generic ParseBoolText -> lower performance but less code redundancy
-    private static JsonResult<JsonBool> ParseBoolText(ReadOnlySpan<char> jsonText, int currentIndex, string boolText)
-    {
-        var boolTextLength = boolText.Length;
-        
-        if (currentIndex + boolTextLength > jsonText.Length)
-        {
-            return JsonResult<JsonBool>.Err(JsonErrorType.EndOfFile, currentIndex);
-        }
-
-        var possibleBoolSlice = jsonText.Slice(currentIndex, boolTextLength);
-
-        return possibleBoolSlice switch
-        {
-            "true" => JsonResult<JsonBool>.Ok(new JsonBool(true), currentIndex + boolTextLength),
-            "false" => JsonResult<JsonBool>.Ok(new JsonBool(false), currentIndex + boolTextLength),
-            _ => JsonResult<JsonBool>.Err(
-                JsonErrorType.InvalidSyntax, $"Expected '{boolText}', received '{possibleBoolSlice}'", currentIndex
-            )
-        };
-    }
-    */
-    
-    private static JsonResult<JsonBool> ParseBool(ReadOnlySpan<char> jsonText, int currentIndex)
+    private static JsonResult<JsonBool> ParseBool(ReadOnlySpan<byte> jsonText, int currentIndex)
     {
         var firstCharacter = jsonText[currentIndex];
 
         return firstCharacter switch
         {
-            't' => ParseTrue(jsonText, currentIndex), // ParseBoolText(jsonText, currentIndex, "true")
-            'f' => ParseFalse(jsonText, currentIndex),  // ParseBoolText(jsonText, currentIndex, "false")
+            (byte)'t' => ParseTrue(jsonText, currentIndex),
+            (byte)'f' => ParseFalse(jsonText, currentIndex),
             _ => JsonResult<JsonBool>.Err(
                 JsonErrorType.InvalidSyntax, $"Expected 't' (true) or 'f' (false), received '{firstCharacter}'",
                 currentIndex
@@ -149,22 +115,22 @@ public static class JsonParser
         };
     }
     
-    private static JsonResult<JsonNumber> ParseNumber(ReadOnlySpan<char> jsonText, int currentIndex)
+    private static JsonResult<JsonNumber> ParseNumber(ReadOnlySpan<byte> jsonText, int currentIndex)
     {
         return JsonResult<JsonNumber>.Ok(new JsonNumber(1), currentIndex);
     }
     
-    private static JsonResult<JsonString> ParseString(ReadOnlySpan<char> jsonText, int currentIndex)
+    private static JsonResult<JsonString> ParseString(ReadOnlySpan<byte> jsonText, int currentIndex)
     {
         return JsonResult<JsonString>.Ok(new JsonString(""), currentIndex);
     }
     
-    private static JsonResult<JsonArray> ParseArray(ReadOnlySpan<char> jsonText, int currentIndex)
+    private static JsonResult<JsonArray> ParseArray(ReadOnlySpan<byte> jsonText, int currentIndex)
     {
         return JsonResult<JsonArray>.Ok(new JsonArray([]), currentIndex);
     }
     
-    private static JsonResult<JsonObject> ParseObject(ReadOnlySpan<char> jsonText, int currentIndex)
+    private static JsonResult<JsonObject> ParseObject(ReadOnlySpan<byte> jsonText, int currentIndex)
     {
         return JsonResult<JsonObject>.Ok(new JsonObject([]), currentIndex);
     }
