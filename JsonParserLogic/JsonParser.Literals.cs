d5272c0bcd9b86ff2315bcab81a5456dde423b27
b-fuhrer <b-fuhrer@users.noreply.github.com>
2026-01-10 20:34:44 +0100

    refactor: implement generic ParseLiteral for JSON literals (null, true, false)

--- JsonParserLogic/JsonParser.Literals.cs
+++ JsonParserLogic/JsonParser.Literals.cs
@@ -1,46 +1,13 @@
 ﻿using System.Text;
 using PracticeJsonParser.Types;
+
 namespace PracticeJsonParser;
 
 public static partial class JsonParser
 {
     internal static JsonResult<JsonNull> ParseNull(ReadOnlySpan<byte> jsonText, int currentIndex)
     {
-        const int nullLength = 4;
-
-        if (currentIndex + nullLength > jsonText.Length)
-        {
-            return JsonResult<JsonNull>.Err(JsonErrorType.EndOfFile, currentIndex);
-        }
-
-        var slice = jsonText.Slice(currentIndex, nullLength);
-
-        if (!slice.SequenceEqual("null"u8))
-        {
-            return JsonResult<JsonNull>.Err(
-                JsonErrorType.InvalidSyntax,
-                $"Expected 'null', received '{Encoding.UTF8.GetString(slice)}'",
-                currentIndex
-            );
-        }
-
-        int afterNullIndex = currentIndex + nullLength;
-
-        if (afterNullIndex == jsonText.Length)
-        {
-            return JsonResult<JsonNull>.Ok(JsonNull.Instance, afterNullIndex);
-        }
-
-        return jsonText[afterNullIndex] switch
-        {
-            (byte)',' or (byte)']' or (byte)'}' or (byte)' ' or (byte)'\t' or (byte)'\n' or (byte)'\r' =>
-                JsonResult<JsonNull>.Ok(JsonNull.Instance, afterNullIndex),
-            _ => JsonResult<JsonNull>.Err(
-                JsonErrorType.InvalidSyntax,
-                "Bools are not allowed to be followed by trailing garbage.",
-                afterNullIndex
-            )
-        };
+        return ParseLiteral(jsonText, currentIndex, "null"u8, JsonNull.Instance);
     }
 
     internal static JsonResult<JsonBool> ParseBool(ReadOnlySpan<byte> jsonText, int currentIndex)
@@ -49,8 +16,8 @@ public static partial class JsonParser
 
         return firstCharacter switch
         {
-            (byte)'t' => ParseTrue(jsonText, currentIndex),
-            (byte)'f' => ParseFalse(jsonText, currentIndex),
+            (byte)'t' => ParseLiteral(jsonText, currentIndex,"true"u8, JsonBool.True),
+            (byte)'f' => ParseLiteral(jsonText, currentIndex, "false"u8, JsonBool.False),
             _ => JsonResult<JsonBool>.Err(
                 JsonErrorType.InvalidSyntax,
                 $"Expected 't' (true) or 'f' (false), received '{firstCharacter}'",
@@ -59,81 +26,45 @@ public static partial class JsonParser
         };
     }
 
-    private static JsonResult<JsonBool> ParseTrue(ReadOnlySpan<byte> jsonText, int currentIndex)
+    private static JsonResult<TLiteral> ParseLiteral<TLiteral>(
+        ReadOnlySpan<byte> jsonText,
+        int currentIndex,
+        ReadOnlySpan<byte> expectedLiteral,
+        TLiteral successReturnValue
+    )
+        where TLiteral : JsonValue
     {
-        const int trueLength = 4;
-
-        if (currentIndex + trueLength > jsonText.Length)
+        if (currentIndex + expectedLiteral.Length > jsonText.Length)
         {
-            return JsonResult<JsonBool>.Err(JsonErrorType.EndOfFile, currentIndex);
+            return JsonResult<TLiteral>.Err(JsonErrorType.EndOfFile, currentIndex);
         }
 
-        var slice = jsonText.Slice(currentIndex, trueLength);
-
-        if (!slice.SequenceEqual("true"u8))
+        var slice = jsonText.Slice(currentIndex, expectedLiteral.Length);
+        if (!slice.SequenceEqual(expectedLiteral))
         {
-            return JsonResult<JsonBool>.Err(
+            return JsonResult<TLiteral>.Err(
                 JsonErrorType.InvalidSyntax,
-                $"Expected 'true', received '{Encoding.UTF8.GetString(slice)}'",
+                $"Expected '{Encoding.UTF8.GetString(expectedLiteral)}', received '{Encoding.UTF8.GetString(slice)}'",
                 currentIndex
             );
         }
 
-        int afterTrueIndex = currentIndex + trueLength;
-
-        if (afterTrueIndex == jsonText.Length)
+        int afterLiteralIndex = currentIndex + expectedLiteral.Length;
+        if (afterLiteralIndex == jsonText.Length)
         {
-            return JsonResult<JsonBool>.Ok(JsonBool.True, afterTrueIndex);
+            return JsonResult<TLiteral>.Ok(successReturnValue, afterLiteralIndex);
         }
 
-        return jsonText[afterTrueIndex] switch
-        {
-            (byte)',' or (byte)']' or (byte)'}' or (byte)' ' or (byte)'\t' or (byte)'\n' or (byte)'\r' =>
-                JsonResult<JsonBool>.Ok(JsonBool.True, afterTrueIndex),
-            _ => JsonResult<JsonBool>.Err(
+        byte afterLiteralCharacter = jsonText[afterLiteralIndex];
+        bool isSeparator = afterLiteralCharacter is (byte)',' or (byte)']' or (byte)'}' or (byte)' ' or (byte)'\t'
+            or (byte)'\n' or (byte)'\r';
+
+        return isSeparator
+            ? JsonResult<TLiteral>.Ok(successReturnValue, afterLiteralIndex)
+            : JsonResult<TLiteral>.Err(
                 JsonErrorType.InvalidSyntax,
-                "Bools are not allowed to be followed by trailing garbage.",
-                afterTrueIndex
-            )
-        };
-    }
-
-    private static JsonResult<JsonBool> ParseFalse(ReadOnlySpan<byte> jsonText, int currentIndex)
-    {
-        const int falseLength = 5;
-
-        if (currentIndex + falseLength > jsonText.Length)
-        {
-            return JsonResult<JsonBool>.Err(JsonErrorType.EndOfFile, currentIndex);
-        }
-
-        var slice = jsonText.Slice(currentIndex, falseLength);
-
-        if (!slice.SequenceEqual("false"u8))
-        {
-            return JsonResult<JsonBool>.Err(
-                JsonErrorType.InvalidSyntax,
-                $"Expected 'false', received '{Encoding.UTF8.GetString(slice)}'",
-                currentIndex
+                $"{(successReturnValue == JsonNull.Instance ? "Null is" : "Bools are")} not allowed to be followed by trailing garbage",
+                afterLiteralIndex
             );
-        }
-
-        int afterFalseIndex = currentIndex + falseLength;
-
-        if (afterFalseIndex == jsonText.Length)
-        {
-            return JsonResult<JsonBool>.Ok(JsonBool.False, afterFalseIndex);
-        }
-
-        return jsonText[afterFalseIndex] switch
-        {
-            (byte)',' or (byte)']' or (byte)'}' or (byte)' ' or (byte)'\t' or (byte)'\n' or (byte)'\r' =>
-                JsonResult<JsonBool>.Ok(JsonBool.False, afterFalseIndex),
-            _ => JsonResult<JsonBool>.Err(
-                JsonErrorType.InvalidSyntax,
-                "Bools are not allowed to be followed by trailing garbage.",
-                afterFalseIndex
-            )
-        };
     }
 }
